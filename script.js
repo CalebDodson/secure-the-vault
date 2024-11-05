0ffb8264a31e997097a11270b235d722e891c862
Caleb Dodson <caleb.v.dodson@gmail.com>
2024-11-05 01:27:12 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Variables for combination
+const number1 = 50 / 5;
+const number2 = 20 * 2;
+const number3 = 27 + 12;
+
+const combination = `${number1}-${number2}-${number3}`;
+
+// Variable for message string
+const message = `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${combination}`;
+
+// Pop-up dialogue to deliver combination
+alert(message);
